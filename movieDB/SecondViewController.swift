de4713d6d03a896a06dc8611d62d11f766a2265e
Kasım SAĞIR <kasim.sagir@hibizz.com>
2019-08-01 18:48:05 +0300

    Initial Commit

---
+++ movieDB/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  movieDB
+//
+//  Created by Kasım on 1.08.2019.
+//  Copyright © 2019 KS. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
